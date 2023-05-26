f2304439b509895d4245866f89e44c0efab023d5
Farhan <farhanahmed2k177@gmail.com>
2023-05-26 18:30:07 +0500

    Add

---
+++ src/Components/Avatar.jsx
@@ -0,0 +1,10 @@
+export default function Avatar() {
+    return (
+      <img
+        src="https://picsum.photos/20/30"
+        width={2150}
+        height={150}
+        alt="Avatar"
+      />
+    );
+  }
---
+++ src/Components/Newc.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+
+
+
+function Newc({someProp}) {
+  return (
+    <div>
+        <h1>Newc</h1>
+
+    </div>
+  )
+}
+
+export default Newc
